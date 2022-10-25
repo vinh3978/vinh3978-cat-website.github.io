da366cb23e7ca01cfde81090f594d66dcbc6b3ae
vinh3978 <114040054+vinh3978@users.noreply.github.com>
2022-10-25 08:28:22 -0400

    version 2.0

---
+++ script.js
@@ -0,0 +1,8 @@
+const imgs = [...document.querySelectorAll('img')];
+console.log("Test:")
+imgs.forEach((item) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('unactive');
+  })
+})
+
